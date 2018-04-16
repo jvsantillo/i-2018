d89610febe8f7a5843bdd9946957886b90f5bfcb
João Vitor Rodrigues Santillo <jvsantillo@gmail.com>
2018-04-16 19:52:40 -0300

    Reorganização e exercício 1 da lista serializacao-java

---
+++ serializacao-java/src/serializacao/java/ContaBancaria.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializacao.java;
+
+
+
+
+
+/**
+ *
+ * @author Alunoinf_2
+ */
+public class ContaBancaria{
+    
+    private double saldo;
+    private int tipo;
+    
+    public ContaBancaria(float saldo, int tipo){
+        this.saldo = saldo;
+        this.tipo = tipo;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    
+    public void deposito(double valor){
+        setSaldo(this.saldo + valor);
+    }
+    
+    public double retornaSaldo(){
+        return this.saldo;
+    }
+    
+    public void saque(double valor){
+        setSaldo(this.saldo - valor);
+    }
+    
+}
---
+++ serializacao-java/src/serializacao/java/SerializacaoJava.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package serializacao.java;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+/**
+ *
+ * @author Alunoinf_2
+ */
+public class SerializacaoJava {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        ContaBancaria conta01 = new ContaBancaria(1950, 1);
+        
+        try
+      {
+          
+         FileOutputStream fileOut = new FileOutputStream("C:\\Users\\Alunoinf_2\\Desktop\\ObjetoSerializado\\objeto_serializado.txt");
+         ObjectOutputStream out = new ObjectOutputStream(fileOut);
+         out.writeObject(conta01);
+         out.close();
+         fileOut.close();
+         System.out.printf("Os dados serializados da conta bancária estão salvos em: C:\\Users\\Alunoinf_2\\Desktop\\ObjetoSerializado\\objeto_serializado.txt");
+         
+      }catch(Exception e)
+      {
+          e.printStackTrace();
+      }
+        
+    }
+    
+}
