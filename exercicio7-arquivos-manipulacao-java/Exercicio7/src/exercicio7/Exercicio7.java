8d8b339d24803a536a74b616a28d61f06eafafda
João Vitor Rodrigues Santillo <jvsantillo@gmail.com>
2018-04-16 21:52:03 -0300

    Exercicio 7 da lista 03

---
+++ exercicio7-arquivos-manipulacao-java/Exercicio7/src/exercicio7/Exercicio7.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercicio7;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ *
+ * @author Alunoinf_2
+ */
+public class Exercicio7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws MalformedURLException {
+        String request = "http://tests.felipesilveira.com.br/android-core/insert.php";
+        URL url = new URL(request); 
+        
+        
+
+    }
+    
+}
